726d8f5b3b4ba835128a9fa7514cc8192ecb4dc2
david fletcher <davidfletcheraaron@gmail.com>
2022-02-01 16:03:49 -0500

    users are working to login and register

--- api/server.js
+++ api/server.js
@@ -3,26 +3,36 @@ const helmet = require('helmet')
 const cors = require('cors')
 const db = require('./data/db-config')
 
-function getAllUsers() { return db('users') }
+const plantsRouter = require('./plants/plants-router')
+const authRouter = require('./auth/auth-router')
+const usersRouter = require('./users/users-router')
 
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+// function getAllUsers() { return db('users') }
+
+// async function insertUser(user) {
+//   // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
+//   // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
+//   const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
+//   return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
+// }
 
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
-server.get('/api/users', async (req, res) => {
-  res.json(await getAllUsers())
-})
+server.use('/api/users', usersRouter)
+server.use('/api/auth', authRouter )
+// server.use('/api/plants', plantsRouter )
 
-server.post('/api/users', async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
-})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+     message: err.message,
+     stack: err.stack,
+  });
+});
+
+
 
 module.exports = server
