3bf5f8a28a569e5070ac12d3cdba6bbe19b56e98
david fletcher <davidfletcheraaron@gmail.com>
2022-07-12 01:29:46 -0400

    users seed includes some companies names(?)

--- api/data/seeds/01-users.js
+++ api/data/seeds/01-users.js
@@ -11,7 +11,9 @@ exports.seed = function(knex) {
         { role_id: 1, username: 'david', password: '$2a$08$dyA1S.XdgJYbzu8XxIHVdORRN5jbcPc1hgKRlZsta2Hr93lh34TKy' },
         { role_id: 2, username: 'sara', password: '$2a$08$dyA1S.XdgJYbzu8XxIHVdORRN5jbcPc1hgKRlZsta2Hr93lh34TKy' },
         { role_id: 2, username: 'debra', password: '$2a$08$dyA1S.XdgJYbzu8XxIHVdORRN5jbcPc1hgKRlZsta2Hr93lh34TKy' },
-        { role_id: 2, username: 'fletcher', password: '$2a$08$dyA1S.XdgJYbzu8XxIHVdORRN5jbcPc1hgKRlZsta2Hr93lh34TKy' }
+        { role_id: 2, username: 'fletcher', password: '$2a$08$dyA1S.XdgJYbzu8XxIHVdORRN5jbcPc1hgKRlZsta2Hr93lh34TKy' },
+        { role_id: 3, username: 'MS', password: '$2a$08$dyA1S.XdgJYbzu8XxIHVdORRN5jbcPc1hgKRlZsta2Hr93lh34TKy' },
+        { role_id: 3, username: 'JPMC', password: '$2a$08$dyA1S.XdgJYbzu8XxIHVdORRN5jbcPc1hgKRlZsta2Hr93lh34TKy' },
       ]);
     });
 };
